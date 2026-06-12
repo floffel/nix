bd4e8801addd3a90339f022b7c2c4e3116d23dc0
Florian Minnecker <florian@minnecker.com>
2026-06-12 20:56:17 +0200

    feat: move forward zones to git under nixnsd/zones and load them declaratively

--- nixnsd/nsd.nix
+++ nixnsd/nsd.nix
@@ -1,6 +1,50 @@
 # NixOS Service Configuration for NSD (Authoritative Nameserver)
 { config, pkgs, lib, ... }:
 
+let
+  # Common secondary nameservers for zone transfers and notifications
+  commonProvideXFR = [
+    "213.239.242.238 NOKEY"              # ns1.first-ns.de
+    "213.133.100.103 NOKEY"              # robotns2.second-ns.de
+    "193.47.99.3 NOKEY"                  # robotns3.second-ns.com
+    "2a01:4f8:0:a101::a:1 NOKEY"         # ns1.first-ns.de IPv6
+    "2a01:4f8:0:1::5ddc:2 NOKEY"         # robotns2.second-ns.de IPv6
+    "2001:67c:192c::add:a3 NOKEY"        # robotns3.second-ns.com IPv6
+    
+    # TSIG-authenticated transfer blocks
+    "213.239.242.238 hetzner-key"
+    "213.133.100.103 hetzner-key"
+    "193.47.99.3 hetzner-key"
+    "2a01:4f8:0:a101::a:1 hetzner-key"
+    "2a01:4f8:0:1::5ddc:2 hetzner-key"
+    "2001:67c:192c::add:a3 hetzner-key"
+
+    # User-defined sync secondary transfer blocks
+    "78.47.124.81 sync"
+    "2a01:4f8:c0c:2ea9::2 sync"
+  ];
+
+  commonNotify = [
+    "213.239.242.238 NOKEY"
+    "213.133.100.103 NOKEY"
+    "193.47.99.3 NOKEY"
+    "2a01:4f8:0:a101::a:1 NOKEY"
+    "2a01:4f8:0:1::5ddc:2 NOKEY"
+    "2001:67c:192c::add:a3 NOKEY"
+    
+    # TSIG-authenticated notify blocks
+    "213.239.242.238 hetzner-key"
+    "213.133.100.103 hetzner-key"
+    "193.47.99.3 hetzner-key"
+    "2a01:4f8:0:a101::a:1 hetzner-key"
+    "2a01:4f8:0:1::5ddc:2 hetzner-key"
+    "2001:67c:192c::add:a3 hetzner-key"
+
+    # User-defined sync secondary notify blocks
+    "78.47.124.81@53 sync"
+    "2a01:4f8:c0c:2ea9::2@53 sync"
+  ];
+in
 {
   services.nsd = {
     enable = true;
@@ -21,70 +65,27 @@
       };
     };
 
-    # Declarative DNS zones
+    # Declarative DNS zones loaded from version-controlled git zone files
     zones = {
       "minnecker.com" = {
-        # Allow Hetzner's and user's secondary nameservers to fetch zones via AXFR/IXFR
-        provideXFR = [
-          "213.239.242.238 NOKEY"              # ns1.first-ns.de
-          "213.133.100.103 NOKEY"              # robotns2.second-ns.de
-          "193.47.99.3 NOKEY"                  # robotns3.second-ns.com
-          "2a01:4f8:0:a101::a:1 NOKEY"         # ns1.first-ns.de IPv6
-          "2a01:4f8:0:1::5ddc:2 NOKEY"         # robotns2.second-ns.de IPv6
-          "2001:67c:192c::add:a3 NOKEY"        # robotns3.second-ns.com IPv6
-          
-          # TSIG-authenticated transfer blocks
-          "213.239.242.238 hetzner-key"
-          "213.133.100.103 hetzner-key"
-          "193.47.99.3 hetzner-key"
-          "2a01:4f8:0:a101::a:1 hetzner-key"
-          "2a01:4f8:0:1::5ddc:2 hetzner-key"
-          "2001:67c:192c::add:a3 hetzner-key"
-
-          # User-defined sync secondary transfer blocks
-          "78.47.124.81 sync"
-          "2a01:4f8:c0c:2ea9::2 sync"
-        ];
-
-        # Notify secondary nameservers when the zone is updated
-        notify = [
-          "213.239.242.238 NOKEY"
-          "213.133.100.103 NOKEY"
-          "193.47.99.3 NOKEY"
-          "2a01:4f8:0:a101::a:1 NOKEY"
-          "2a01:4f8:0:1::5ddc:2 NOKEY"
-          "2001:67c:192c::add:a3 NOKEY"
-          
-          # TSIG-authenticated notify blocks
-          "213.239.242.238 hetzner-key"
-          "213.133.100.103 hetzner-key"
-          "193.47.99.3 hetzner-key"
-          "2a01:4f8:0:a101::a:1 hetzner-key"
-          "2a01:4f8:0:1::5ddc:2 hetzner-key"
-          "2001:67c:192c::add:a3 hetzner-key"
-
-          # User-defined sync secondary notify blocks
-          "78.47.124.81@53 sync"
-          "2a01:4f8:c0c:2ea9::2@53 sync"
-        ];
-
-        # Initial placeholder zone file content to allow nameserver bootup
-        data = ''
-          $TTL 3600
-          @ IN SOA ns1.minnecker.com. admin.minnecker.com. (
-              2026061201 ; serial (YYYYMMDDNN)
-              86400      ; refresh (24 hours)
-              7200       ; retry (2 hours)
-              3600000    ; expire (1000 hours)
-              3600       ; minimum (1 hour)
-          )
-          @ IN NS ns1.minnecker.com.
-          @ IN NS robotns2.second-ns.de.
-          @ IN NS robotns3.second-ns.com.
-          @ IN NS ns4.minnecker.com.
-          ns1 IN A 172.16.16.21
-          ns4 IN A 78.47.124.81
-        '';
+        provideXFR = commonProvideXFR;
+        notify = commonNotify;
+        data = builtins.readFile ./zones/minnecker.com.forward;
+      };
+      "floffel.de" = {
+        provideXFR = commonProvideXFR;
+        notify = commonNotify;
+        data = builtins.readFile ./zones/floffel.de.forward;
+      };
+      "sbminnecker.de" = {
+        provideXFR = commonProvideXFR;
+        notify = commonNotify;
+        data = builtins.readFile ./zones/sbminnecker.de.forward;
+      };
+      "substitution.art" = {
+        provideXFR = commonProvideXFR;
+        notify = commonNotify;
+        data = builtins.readFile ./zones/substitution.art.forward;
       };
     };
   };

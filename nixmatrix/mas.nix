7254018aa25167b6ad6fd6bc6a17650b3358fb3f
Florian Minnecker <florian.git@minnecker.com>
2026-08-03 23:27:48 +0200

    matrix: quote Tera split arg with single quotes and drop trailing newline
    The localpartTemplate value is interpolated into a YAML double-quoted
    scalar (template: "..."). split(pat="@") embedded a " that closed
    the YAML scalar early, breaking secrets.yaml parsing. Use Tera
    single-quoted '@' (supported by Tera) so no " lands inside the value.
    
    Also switch from a Nix indented '' string to a single-line string: the
    indented form added a trailing newline that YAML folds into a trailing
    space, which would have produced Matrix localparts ending in a space.

--- nixmatrix/mas.nix
+++ nixmatrix/mas.nix
@@ -4,9 +4,7 @@ let
   package = pkgs.matrix-authentication-service;
   synapseClientId = "0000000000000000000SYNAPSE";
   upstreamProviderId = "01J8QGXVJHSKAB1JFJYF2TBBDD";
-  localpartTemplate = ''
-    {{ user.preferred_username | split(pat="@") | first | lower }}
-    '';
+  localpartTemplate = "{{ user.preferred_username | split(pat='@') | first | lower }}";
 
   configYaml = pkgs.formats.yaml { };
 

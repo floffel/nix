549e5a66dcade5f1640062b654ac9534b655e71a
Florian Minnecker <florian.git@minnecker.com>
2026-08-04 11:28:40 +0200

    livekit: add missing livekitUrl setting to lk-jwt-service

--- nixnginx/nginx.nix
+++ nixnginx/nginx.nix
@@ -1233,6 +1233,7 @@ systemd.services.nextcloud-setup.unitConfig = { };
   services.lk-jwt-service = {
     enable = true;
     port = 8082;
+    livekitUrl = "http://127.0.0.1:7880";
     keyFile = "/var/lib/livekit/keys.yaml";
   };
 
